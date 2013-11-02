1426846008cbcf36ae7b6543a6a8d26ad2c4d921
Darkdadaah <darkdadaah@yahoo.fr>
2013-11-02 02:06:13 +0100

    Use parse_dump and distinguish redirections - Parse_dump makes things more easier - Now all redirections can be simply extracted - The redirections are removed from the lists by default
    Change-Id: Ib38eca8e7d4adc00253797beb1a874c0b4c790db

--- scripts/wikt_dumps_diff.pl
+++ scripts/wikt_dumps_diff.pl
@@ -7,7 +7,7 @@ use Getopt::Std;
 
 use lib '..';
 use wiktio::basic		qw(step stepl print_value dump_input);
-use wiktio::parser		qw(parseArticle);
+use wiktio::parser		qw(parse_dump parseArticle);
 our %opt;
 
 # Special case of Wiktionaries that use full name or special codes for their langues
@@ -60,6 +60,8 @@ sub usage
 	-o <path> : output path of the pages found only in the first Wiktionary
 	-O <path> : output path of the pages found only in the second Wiktionary
 	
+	-R		  : disable redirect matching
+	
 	example: $0 -c en -l fr -L en -i frwikt.xml -I enwikt.xml -o en_frwikt-only.txt -O en_enwikt_only.txt
 EOF
 	exit;
@@ -69,7 +71,7 @@ EOF
 # Command line options processing
 sub init()
 {
-	getopts( 'hc:l:L:i:I:o:O:', \%opt ) or usage();
+	getopts( 'hc:l:L:i:I:o:O:R', \%opt ) or usage();
 	usage() if $opt{h};
 	
 	usage( "Language to compare needed (-c)" ) if not $opt{c};
@@ -118,76 +120,74 @@ sub prepare_language_section
 # Parse a dump
 sub get_articles_list
 {
-	my ($file, $lang_section) = @_;
+	my ($dump_path, $lang_section) = @_;
 	my $list = {};
+	my $redirect = {};
 	
-	open(DUMP, dump_input($file)) or die "Couldn't open '$file': $!\n";
+	open(my $dump_fh, dump_input($dump_path)) or die "Couldn't open '$dump_path': $!\n";
 	
-	my $title = '';
-	while(my $line = <DUMP>) {
-		# Get page title
-		if ($line =~ /<title>(.+?)<\/title>/) {
-			$title = $1;
-			# Exclude pages outside of the main space
-			$title = '' if $title =~ /[:\/]/;
+	ARTICLE : while(my $article = parse_dump($dump_fh)) {
+		if (not defined($article->{'fulltitle'})) {
+			next ARTICLE;
+		}
 		
-		# Get page content
-		} elsif ($title and $line =~ /<text xml:space="preserve">(.*?)$/) {
-			my $head = $1;
-			# Search for the language section already
-			if ($head =~ /$lang_section/) {
-				$list->{$title} = 1;
-				$title = '';
-				next;
-			} else {
-				if ($line =~ /<\/text>/) {
-					$title='';
-					next;
-				}
-				while (my $inline = <DUMP>) {
-					# Continue to search for the language section
-					if ($inline =~ /$lang_section/) {
-						$list->{$title} = 1;
-						$title='';
-					}
-					if ($inline =~ /<\/text>/) {
-						$title='';
-						last;
-					}
-				}
+		if (defined($article->{'redirect'})) {
+			$redirect->{ $article->{'title'} }++;
+		}
+		
+		if ($article->{'ns'} != 0) {
+			next ARTICLE;
+		}
+		
+		# Search for the language section
+		foreach my $line (@{ $article->{'content'} }) {
+			if ($line =~ /$lang_section/) {
+				$list->{ $article->{'title'} }++;
+				next ARTICLE;
 			}
 		}
 	}
-	close(DUMP);
-	
-	return $list;
+	return $list, $redirect;
 }
 
 # Compare both lists and outputs items that are specific to each and common to both
 sub diff_lists
 {
-	my ($first, $second) = @_;
-	my $first_only = {};
-	my $second_only = {};
+	my ($art1, $art2, $red1, $red2) = @_;
+	my $art1_only = {};
+	my $art2_only = {};
 	my $common = {};
+	my $redscore = 0;
 	
-	foreach my $one (keys %$first) {
-		if ($second->{$one}) {
-			$common->{$one} = 1;
-			delete $first->{$one};
-			delete $second->{$one};
+	foreach my $title (keys %$art1) {
+		if ($art2->{$title}) {
+			$common->{$title} = 1;
+			delete $art1->{$title};
+			delete $art2->{$title};
+		} elsif ($red2->{$title}) {
+			$common->{$title} = 1;
+			delete $art1->{$title};
+			delete $red2->{$title};
+			$redscore++;
 		} else {
-			$first_only->{$one} = 1;
-			delete $first->{$one};
+			$art1_only->{$title} = 1;
+			delete $art1->{$title};
 		}
 	}
 	
-	foreach my $two (keys %$second) {
-		$second_only->{$two} = 1;
-		delete $second->{$two};
+	foreach my $title (keys %$art2) {
+		if ($red1->{$title}) {
+			$common->{$title} = 1;
+			delete $art2->{$title};
+			delete $red1->{$title};
+			$redscore++;
+		} else {
+			$art2_only->{$title} = 1;
+			delete $art2->{$title};
+		}
 	}
 	
-	return ($first_only, $second_only, $common);
+	return ($art1_only, $art2_only, $common, $redscore);
 }
 
 # Output a list of articles in wiki code with a link to the other wiktionary
@@ -209,23 +209,32 @@ sub write_list
 init();
 
 # Get list from the first Wiktionary
-stepl "Parse $opt{l} Wiktionary: ";
+step "Parse $opt{l} Wiktionary: ";
 my $lang_section_1 = prepare_language_section($opt{l}, $opt{c});
-my $first = get_articles_list($opt{i}, $lang_section_1);
-print_value("%d articles in $opt{c}", $first);
+my ($articles_1, $redirect_1) = get_articles_list($opt{i}, $lang_section_1);
+print_value("\t%d articles in $opt{c}", $articles_1);
+print_value("\t%d redirects", $redirect_1);
 
 # Get list from the second Wiktionary
-stepl "Parse $opt{L} Wiktionary: ";
+step "Parse $opt{L} Wiktionary: ";
 my $lang_section_2 = prepare_language_section($opt{L}, $opt{c});
-my $second = get_articles_list($opt{I}, $lang_section_2);
-print_value("%d articles in $opt{c}", $second);
+my ($articles_2, $redirect_2) = get_articles_list($opt{I}, $lang_section_2);
+print_value("\t%d articles in $opt{c}", $articles_2);
+print_value("\t%d redirects", $redirect_2);
+
+# Disable redirect matching?
+if ($opt{R}) {
+	$redirect_1 = {};
+	$redirect_2 = {};
+}
 
 # Compare
 step "Compare:";
-my ($first_only, $second_only, $common) = diff_lists($first, $second);
-print_value("%d articles only in $opt{l}", $first_only);
-print_value("%d articles only in $opt{L}", $second_only);
-print_value("%d articles in common", $common);
+my ($first_only, $second_only, $common, $redirect_count) = diff_lists($articles_1, $articles_2, $redirect_1, $redirect_2);
+print_value("\t%d articles only in $opt{l}", $first_only);
+print_value("\t%d articles only in $opt{L}", $second_only);
+print_value("\t%d articles in common", $common);
+print_value("\t%d articles matched to redirects", $redirect_count);
 
 # Write lists
 if ($opt{o}) {
