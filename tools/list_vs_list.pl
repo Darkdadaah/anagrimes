9ebf8f7120b45de3c39b3840f863fbb7463c09db
Darkdadaah <darkdadaah@yahoo.fr>
2014-03-30 00:14:06 +0100

    Getopt and more options

--- tools/list_vs_list.pl
+++ tools/list_vs_list.pl
@@ -2,36 +2,75 @@
 
 use strict;
 use warnings;
+use Getopt::Std;
+
 # UTF8
 use open IO => ':utf8';
 binmode STDOUT, ":utf8";
 binmode STDERR, ":utf8";
 use utf8;
 
-my $nodiacritics = 1;
+our %opt;	# Getopt options
+
+#################################################
+# Message about this program and how to use it
+sub usage
+{
+	print STDERR "[ $_[0] ]\n" if $_[0];
+	print STDERR << "EOF";
+	
+	This script compares two words lists.
+	
+	usage: $0 [-h] -f file
+	
+	-h        : this (help) message
+	
+	-r <path> : path to the first list (reference)
+	-t <path> : path to the second list (test)
+
+	Outputs:
+	-o <path> : words from the test not found in the reference
+	
+	Options:
+	-d        : don't take diacritics into account
+	-L        : ignore locutions
+EOF
+	exit;
+}
+
+##################################
+# Command line options processing
+sub init()
+{
+	getopts( 'r:t:o:L', \%opt ) or usage();
+	usage() if $opt{h};
+	usage("No reference file given (-r)") unless $opt{r};
+	usage("No test file given (-t)") unless $opt{t};
+	usage("No output file given (-o)") unless $opt{o};
+}
 
 ####################################################################
 # FUNCTIONS
 
-sub get_words($)
+sub get_words
 {
-        my $file = shift;
+        my ($file, $no_loc) = @_;
         
         my $list = {};
         
         open(FILE, $file) or die("$file: $!");
-        my $code = 'utf8';
         while(<FILE>) {
                 my $line = $_;
                 chomp($line);
                 
                 my ($word) = split(/\t|","/, $line);
                 next if not $word;
+                next if $no_loc and $word =~ / /;
                 $word =~ s/"//g;
-
+				
                 my $key = $word;
                 $key = nodia($key);
-                $list->{$key} = $word;
+                $list->{$key}++;
                 
                 if ($word =~ / /) {
                         my @sub_words = split(' ', $word);
@@ -44,35 +83,35 @@ sub get_words($)
 }
 
 sub nodia {
-        my ($w)  = @_;
+        my ($w, $nodiacritics)  = @_;
         return $w if not $nodiacritics;
         
-        $w =~ s/(â|â|ä)/a/g;
-        $w =~ s/(é|è|ê|ë)/e/g;
-        $w =~ s/(î|ï)/i/g;
-        $w =~ s/(ô|ö)/o/g;
-        $w =~ s/(û|ü)/u/g;
-
         $w = lc($w);
+        $w =~ tr/ââä/aaa/;
+        $w =~ tr/éèêë/eeee/;
+        $w =~ tr/îï/ii/;
+        $w =~ tr/ôö/oo/;
+        $w =~ tr/ûü/uu/;
         
         return $w;
 }
 
 ####################################################################
 # MAIN
+init();
 
-# Get files
-my $refs = $ARGV[0];   die("No reference file provided\n") unless $refs;
-my $test = $ARGV[1];   die("No test file provided\n") unless $test;
+# Get reference word list
+my $rt = get_words($opt{r}, $opt{L});
 
-# Get refs word list
-my $rt = get_words($test);
+{
+	print STDERR keys(%$rt)." mots dans la liste de référence\n";
+}
 
 # Compare every word
-open(REF, $refs) or die("$refs: $!");
+open(REF, $opt{t}) or die("$opt{t}: $!");
+open(OUT, ">$opt{o}") or die("Couldn't write $opt{o}: $!");
 my $nref=0;
 my $missing=0;
-my $code = 'utf8';
 while(<REF>) {
         next if /^\s*$/;
         
@@ -81,28 +120,28 @@ while(<REF>) {
         
         my ($word) = split(/\t|","/, $line);
         $word =~ s/"//g;
+        next if $opt{L} and $word =~ / /;
         
-        my $key = nodia($word);
+        my $key = nodia($word, $opt{d});
         
-        # Not in the test?
+        # Not in the reference?
         if (not $rt->{$key}) {
-                print STDOUT "$word\n";
+                print OUT "$word\n";
                 $missing++;
-        }
+        } else {
+			delete $rt->{$key};
+		}
         $nref++;
 }
+close(OUT);
 close(REF);
 
 {
-        print STDERR "$nref mots dans la liste référence\n";
+	print STDERR "$nref mots dans la liste test\n";
 }
 
 {
-        print STDERR keys(%$rt)." mots dans la liste test\n";
-}
-
-{
-        print STDERR "$missing mots de la référence absents de la liste test\n";
+	print STDERR "$missing mots de la test absents de la liste de référence\n";
 }
 
 
