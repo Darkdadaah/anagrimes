5d34f292fcee5a56611cda07430fa3df78cb149c
Matthieu Barba <matthieu.barba@igmors.u-psud.fr>
2014-08-28 10:22:28 +0200

    rm sql files: schema created dynamically

--- sql/sql_tables_drop_old.sql
+++
@@ -1,3 +0,0 @@
-SELECT '- Drop the old tables' AS '' ;
-DROP TABLE articles_old, mots_old, langues_old ;
-
--- sql/sql_tables_move_back.sql
+++
@@ -1,8 +0,0 @@
-SELECT '- Move the old tables back' AS '' ;
-RENAME TABLE    articles TO articles_temp,
-                articles_old TO articles,
-		mots TO mots_temp,
-                mots_old TO mots,
-		langues TO langues_temp,
-                langues_old TO langues ;
-
