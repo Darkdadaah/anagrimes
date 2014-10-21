a0afe1c36177ae9efc27c5c7185af35e279d081a
Darkdadaah <darkdadaah@yahoo.fr>
2014-10-21 22:11:33 +0200

    Split files by first letter

--- tools/decoupe.pl
+++ tools/decoupe.pl
@@ -3,6 +3,12 @@
 use strict;
 use warnings;
 use Getopt::Std;
+use lib '..';
+use wiktio::string_tools	qw(ascii);
+use utf8;
+use open IO => ':utf8';
+binmode STDOUT, ":utf8";
+binmode STDERR, ":utf8";
 
 our %opt;	# Getopt options
 
@@ -21,6 +27,7 @@ sub usage
 	
 	-i <path> : path to the list to split
 	-l <int>  : max number of elements in each subfile
+	-A        : try to cut first letters together (-l needed)
 
 	Optional:
 	-H        : keep the first line in every file
@@ -33,15 +40,15 @@ EOF
 # Command line options processing
 sub init()
 {
-	getopts( 'i:l:HT', \%opt ) or usage();
+	getopts( 'i:l:HTA', \%opt ) or usage();
 	usage() if $opt{h};
 	usage("No file given (-i)") unless $opt{i};
-	usage("No max length given (-l)") unless ($opt{l} and $opt{l} > 0);
+	usage("Max length or alphabet order to give (-l|-A)") unless ($opt{l} and $opt{l} > 0 or $opt{A});
 }
 
 sub split_file
 {
-	my ($list_path, $max, $head, $tail) = @_;
+	my ($list_path, $max, $head, $tail, $alphabet) = @_;
 
 
 	open(LIST, "<$list_path") or die("Couldn't read list file $list_path: $!");
@@ -74,35 +81,52 @@ sub split_file
 	
 	# Read the list file...
 	my $last = '';
+	my @files = ($out_path);
+	my $letter = '';
 	while (my $line = <LIST>) {
+		next if $line =~ /^\s*$/;
+		# Find first letter
+		my $cur_letter = '';
+		my $title = $line;
+		if ($title =~ /^([^\]]+\]\]).*$/) {
+			$title = $1;
+		}
+		my $asciized = ascii($title);
+		if ($asciized =~ /^[^\-\p{Letter}]*(\-|\p{Letter})/) {
+			$cur_letter = "\L$1";
+			$cur_letter = $letter if $asciized =~ /\d/;
+		}
+		
 		# If we reached the max number or lines, close the current file
 		# and open a new one with the next file number
-		if ($n >= $max) {
+		if (($max and $n >= $max) or
+				($alphabet and $cur_letter ne $letter)) {
+			warn("$letter -> $cur_letter\n");
 			# Close old file
 			$n=0;
 			close(OUT);
-			
 			# Init new file
 			$num++;
-			my $out_path =  $filename.'_'.$num.$suff;
+			my $id = ($alphabet and $cur_letter) ? $cur_letter : $num;
+			my $out_path = $filename.'_'.$id.$suff;
+			push @files, $out_path;
 			open(OUT, ">$out_path") or die("Couldn't write $out_path: $!");
-			print OUT $first if $opt{H};
+			print OUT $first if $head;
 		}
 		# Continue to write the lines from the original file
 		print OUT $line;
 		$n++;
-		$last = $line;
+		$letter = $cur_letter;
+		$last = $line if not $line =~ /^\s*$/;
 	}
 	close(OUT);
 	close(LIST);
 	
 	# Write the last line for every file (except the last one)
 	if ($tail) {
-		for (my $i=1; $i < $num; $i++) {
-			my $out_path =  $filename.'_'.$i.$suff;
-			
+		foreach my $out_path (@files) {
 			open(OUT, ">>$out_path") or die("Couldn't append to $out_path: $!");
-			print OUT $last;
+			print OUT "$last\n";
 			close(OUT);
 		}
 	}
@@ -114,7 +138,7 @@ sub split_file
 # MAIN
 init();
 
-my $num = split_file($opt{i}, $opt{l}, $opt{H}, $opt{T});
+my $num = split_file($opt{i}, $opt{l}, $opt{H}, $opt{T}, $opt{A});
 print STDERR "$num files created\n";
 
 __END__
