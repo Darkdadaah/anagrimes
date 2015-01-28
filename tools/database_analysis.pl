ac7a16ca4deb409af207d3f3cc41c4ced7342abe
Matthieu Barba <matthieu.barba@igmors.u-psud.fr>
2015-01-28 17:58:18 +0100

    Improve fr pron translitteration (~40%)

--- tools/database_analysis.pl
+++ tools/database_analysis.pl
@@ -50,7 +50,7 @@ sub get_articles_prons
 {
 	my ($dbh, $lang) = @_;
 	
-	my $query = "SELECT a_title, p_pron FROM defentries WHERE l_lang=? AND NOT p_pron IS NULL";
+	my $query = "SELECT a_title, p_pron FROM entries WHERE l_lang=? AND NOT p_pron IS NULL GROUP BY a_title";
 	return $dbh->selectall_arrayref($query, { Slice => {} }, $lang);
 }
 
@@ -59,14 +59,14 @@ sub pronunciations
 	my ($dbh, $lang) = @_;
 	
 	# Get all articles with pronunciations
-	my $articles = get_articles_pron($dbh, $lang);
+	my $articles = get_articles_prons($dbh, $lang);
 	my $n = @$articles;
 	print STDERR "$n articles with pronunciations in lang $lang\n";
 
 	# Compute expected pronunciation based on the language
-	my $diff = expected_pronunciation($articles, $lang);
-	my $ndiff = @$diff;
-	print STDERR "$ndiff articles found where pronunciation is different from expected\n";
+	my $same = expected_pronunciation($articles, $lang);
+	my $nsame = @$same;
+	print STDERR "$nsame articles with expected pronunciation " . sprintf("(%.2f %%)\n", $nsame/$n*100);
 
 	# List all words where the pronunciation is different from expected
 }
@@ -75,11 +75,11 @@ sub expected_pronunciation
 {
 	my ($articles, $lang) = @_;
 	
-	my @diff = ();
+	my @same = ();
 	foreach my $a (@$articles) {
-		push @diff, $a if not check_pronunciation($a, $lang);
+		push @same, $a if check_pronunciation($a, $lang);
 	}
-	return \@diff;
+	return \@same;
 }
 
 sub check_pronunciation
@@ -89,14 +89,15 @@ sub check_pronunciation
 	# Check French
 	if ($lang eq 'fr') {
 		my $pron = pron_in_fr($art->{'a_title'});
-		print STDOUT "$art->{'a_title'}\t$pron\t$art->{p_pron}\n";
 		
-		if (different($art->{p_pron}, $pron)) {
-			return 0;
+		if ($pron) {
+			my $diff = different($art->{p_pron}, $pron);
+			my $diffs = $diff ? ':-(' : 'OK';
+			print STDOUT "$diffs\t$art->{'a_title'}\t'$pron'\t'$art->{p_pron}'\n";
+			return not $diff;
 		} else {
-			return 1;
+			return 0;
 		}
-		return 1;
 	}
 	return 1;
 }
@@ -104,39 +105,143 @@ sub check_pronunciation
 sub different
 {
 	my ($p1, $p2) = @_;
-	return 0;
+	
+	my $pron1 = simple($p1);
+	my $pron2 = simple($p2);
+	
+	if ($pron1 eq $pron2) {
+		return 0;
+	} else {
+		return 1;
+	}
+}
+
+sub simple
+{
+	my ($m) = @_;
+	
+	$m =~ s/[\s\.‿ ]+//g;
+	$m =~ s/ə//g;
+	return $m;
 }
 
 sub pron_in_fr
 {
 	my ($w) = @_;
-
-	my $p = lc($w);
-	$p =~ s/e\b//g;
-	$p =~ s/er\b/e/g;
-	$p =~ s/s?ch/S/g;
-	$p =~ s/([aeouiy])s([aeiouy])/$1z$2/g;
-	$p =~ s/j|ge/Z/g;
-	$p =~ s/qu([ei])/k$1/g;
-	$p =~ s/euill?e?/œj/g;
-	$p =~ s/ueill?e?/œj/g;
-	$p =~ s/eu/2/g;
-	$p =~ s/ill/ij/g;
-	$p =~ s/ou?in/wE~/g;
-	$p =~ s/in([^aeiouéèy])/E~$1/g;
-	$p =~ s/ou/U/g;
-	$p =~ s/à|â/A/g;
-	$p =~ s/e?au|ô/o/g;
-	$p =~ s/ail/Aj/gi;
-	$p =~ s/e?oi/wa/g;
-	$p =~ s/ai/E/g;
-	$p =~ s/u/Y/g;
-	$p =~ s/é/e/g;
-	$p =~ s/ê|è/E/g;
-
-	$p =~ s/i([EAOY])/j$1/g;
 	
-	return $p;
+	if ($w =~ /\b([A-Z\.,;!\(\)+\-]|−)+\b/ or $w =~ /[0-9]/) {
+		return '';
+	}
+	my $p = lc($w);
+	
+	$p =~ s/[\.,;:!\?]//g;
+	$p =~ s/'|’/ /g;
+	
+	my $voy = "eaiuUY";
+	$voy .= "ɛÉÈɛ̃œɑ";
+	$voy .= decode('utf8', "ɛÉÈɛ̃œɑə");
+	my $cons = "bdfGklmnpsStvz";
+	$cons .= "ɡʒʁʃ";
+	$cons .= decode('utf8', "ɡʒʁʃ");
+	
+	# Mots
+	$p =~ s/\b([ldms])es\b/$1ɛ/g;
+	$p =~ s/\b([ldms])e\b/$1ə/g;
+	
+	# Terminaisons courantes
+	$p =~ s/tions?\b/SJɔ̃/g;
+	$p =~ s/(ots?|e?aux?)\b/O/g;
+	$p =~ s/ements?\b/əmɑ̃/g;
+	$p =~ s/ments?\b/mɑ̃/g;
+	$p =~ s/ants?\b/ɑ̃/g;
+	$p =~ s/antes?\b/ɑ̃t/g;
+	$p =~ s/ient?s?\b/Jɛ̃/g;
+	$p =~ s/ets?\b/ɛ/g;
+	$p =~ s/ettes?\b/ɛT/g;
+	$p =~ s/ès\b/ɛS/g;
+	$p =~ s/iers?\b/JÉ/g;
+	$p =~ s/er\b/É/g;
+	
+	$p =~ s/s\b//g;
+	$p =~ s/ç/S/g;
+	
+	# 1 Voyelles
+	$p =~ s/ï/I/g;
+	$p =~ s/é/É/g;
+	$p =~ s/ê|è/ɛ/g;
+	$p =~ s/e([$cons]{2})/ɛ$1/g;
+	$p =~ s/([$voy])s([$voy])/$1z$2/g;	# se -> ze
+	$p =~ s/oy([$voy])/waJ$1/g;
+	$p =~ s/y([$voy])/J$1/g;
+	$p =~ s/y/i/g;
+	$p =~ s/ou?in/wɛ̃/g;
+	$p =~ s/ou([iaɛ̃ɛ])/w$1/g;
+	$p =~ s/oue([$cons])/wɛ$1/g;
+	$p =~ s/oi/wa/g;
+	$p =~ s/euill?e?/œJ/g;
+	$p =~ s/ueill?e?/œJ/g;
+	$p =~ s/ille/iJe/g;
+	$p =~ s/ails?$/ɑJ/gi;
+	$p =~ s/eu[xs]?$/ø/g;
+	$p =~ s/(e|œ)u/œ/g;
+	$p =~ s/ou/U/g;
+	$p =~ s/o([$cons])\1/ɔ$1/g;
+	$p =~ s/on([$cons]|s?\b)/ɔ̃$1/g;
+	$p =~ s/à|â/ɑ/g;
+	$p =~ s/e?au|ô/O/g;
+	$p =~ s/e?oi/wa/g;
+	$p =~ s/o([^$voy])/ɔ$1/g;
+	#$p =~ s/i([$voy])[^es]/j$1/g;
+	$p =~ s/e([$cons])\1/ɛ$1/g;
+	$p =~ s/an([$cons])/ɑ̃$1/g;
+	$p =~ s/(\b|[$cons])(i?)(ain|en|in)h?(\b|[$cons])/$1$2ɛ̃$4/g;
+	$p =~ s/\b(ain|en|in)([$cons])/ɛ̃$2/g;
+	$p =~ s/(\b|[$cons])(i?)(an)(\b|[$cons])/$1$2ɑ̃$4/g;
+	$p =~ s/iɛ̃/Jɛ̃/g;
+	$p =~ s/iɛ/Jɛ/g;
+	$p =~ s/ia/Ja/g;
+	$p =~ s/ai/ɛ/g;
+	
+	# Consonnes
+	$p =~ s/cc/k/g;
+	$p =~ s/cc([aoɔUiuy])/k$1/g;
+	$p =~ s/cc([eÉiJ])/ks$1/g;
+	$p =~ s/c([lraoɔuU])/k$1/g;
+	$p =~ s/c([eÉiJ])/s$1/g;
+	$p =~ s/(sc|c|s)h/ʃ/g;
+	$p =~ s/c/k/g;
+	$p =~ s/ph/f/g;
+	$p =~ s/gn([$voy])/ɲ$1/g;
+	$p =~ s/g([aoɔUiÉ])/ʒ$1/g;
+	$p =~ s/gu/G/g;
+	$p =~ s/j|ge|g$/ʒ/g;
+	$p =~ s/qu([ei])/k$1/g;
+	$p =~ s/x/ks/g;
+	
+	# Doubles
+	$p =~ s/([$cons])\1/$1/g;
+	$p =~ s/([$voy])\1/$1/g;
+	
+	# Derniers
+	$p =~ s/ant?s?\b/ɑ̃/g;
+	$p =~ s/u/Y/g;
+	$p =~ s/([$voy])[ts]\b/$1/g;
+	$p =~ s/([^\b]{2})e /$1 /g;
+	$p =~ s/e\b//g;	# e muet
+	$p =~ s/e/ə/g;	# e caduc
+	$p =~ s/h//g;
+	
+	return SAMPA_API($p);
+}
+
+sub SAMPA_API
+{
+	my ($w) = @_;
+	$w =~ tr/YUIJGOST/yuijgost/;
+	$w =~ s/r/ʁ/g;
+	$w =~ s/g/ɡ/g;
+	$w =~ s/É/e/g;
+	return $w;
 }
 
 sub count_letters
