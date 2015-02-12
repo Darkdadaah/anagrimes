a8af7e918bc1c5a46e65ec581517c5089cd8ace1
Darkdadaah <darkdadaah@yahoo.fr>
2015-02-12 11:20:49 +0100

    Improved pron fr detection (40-50%)

--- tools/database_analysis.pl
+++ tools/database_analysis.pl
@@ -5,8 +5,24 @@ use warnings;
 use Getopt::Std;
 use DBI;
 use Encode;
+use Data::Dumper;
 
 our %opt;	# Getopt options
+
+my $conditions = << "REQ";
+	l_lang=? AND
+	NOT p_pron IS NULL AND
+	NOT l_type="symb" AND
+	NOT l_type="lettre" AND
+	NOT l_type="nom-sciences" AND
+	NOT l_type="nom-pr" AND
+	NOT l_type="nom-fam" AND
+	NOT l_type="prenom"
+REQ
+# AND
+#	NOT l_is_flexion AND
+#	NOT l_is_gentile
+#REQ
 
 #################################################
 # Message about this program and how to use it
@@ -42,7 +58,7 @@ sub get_articles
 {
 	my ($dbh, $lang) = @_;
 	
-	my $query = "SELECT DISTINCT(a_title) FROM articles JOIN lexemes ON a_artid=l_artid WHERE l_lang=?";
+	my $query = "SELECT DISTINCT(a_title) FROM articles JOIN lexemes ON a_artid=l_artid WHERE $conditions";
 	return $dbh->selectcol_arrayref($query, undef, $lang);
 }
 
@@ -50,7 +66,12 @@ sub get_articles_prons
 {
 	my ($dbh, $lang) = @_;
 	
-	my $query = "SELECT a_title, p_pron FROM entries WHERE l_lang=? AND NOT p_pron IS NULL GROUP BY a_title";
+	my $query = << "REQ";
+	SELECT *
+	FROM entries
+	WHERE $conditions
+	GROUP BY a_title
+REQ
 	return $dbh->selectall_arrayref($query, { Slice => {} }, $lang);
 }
 
@@ -88,12 +109,22 @@ sub check_pronunciation
 
 	# Check French
 	if ($lang eq 'fr') {
-		my $pron = pron_in_fr($art->{'a_title'});
+		my $pron = pron_in_fr($art);
 		
 		if ($pron) {
 			my $diff = different($art->{p_pron}, $pron);
-			my $diffs = $diff ? ':-(' : 'OK';
-			print STDOUT "$diffs\t$art->{'a_title'}\t'$pron'\t'$art->{p_pron}'\n";
+			my $diffest = very_different($art->{p_pron}, $pron);
+			my $diffs = '';
+			if ($diff) {
+				if ($diffest) {
+					$diffs = 'nope';
+				} else {
+					$diffs = 'OK?';
+				}
+			} else {
+				$diffs = 'OK';
+			}
+			print STDOUT "$diffs\t$art->{'a_title'}\t'$pron'\t'".clean_pron($art->{p_pron})."'\n";
 			return not $diff;
 		} else {
 			return 0;
@@ -116,116 +147,193 @@ sub different
 	}
 }
 
+sub very_different
+{
+	my ($p1, $p2) = @_;
+	
+	my $pron1 = simplest($p1);
+	my $pron2 = simplest($p2);
+	
+	if ($pron1 eq $pron2) {
+		return 0;
+	} else {
+		return 1;
+	}
+}
+
+
 sub simple
 {
 	my ($m) = @_;
 	
-	$m =~ s/[\s\.‿ ]+//g;
+	$m =~ s/([\s\.‿  ])+//g;
 	$m =~ s/ə//g;
 	return $m;
 }
 
+sub simplest
+{
+	my ($m) = @_;
+	
+	$m = simple($m);
+	$m =~ s/ɑ/a/g;
+	$m =~ s/ɔ/o/g;
+	$m =~ s/ɛ/e/g;
+	$m =~ s/ʁ/r/g;
+	$m =~ s/ɡ/g/g;
+	$m =~ s/(.)\1/$1/g;
+	return $m;
+}
+
+sub clean_pron
+{
+	my ($pron) = @_;
+	$pron =~ s/[\.,;:!\?]//g;
+	$pron =~ s/'|’/ /g;
+	return $pron;
+}
+
 sub pron_in_fr
 {
-	my ($w) = @_;
+	my ($art) = @_;
+	my $w = $art->{'a_title'};
+	my $typ = $art->{'l_type'} ? $art->{'l_type'} : '';
 	
 	if ($w =~ /\b([A-Z\.,;!\(\)+\-]|−)+\b/ or $w =~ /[0-9]/) {
 		return '';
 	}
 	my $p = lc($w);
+	$p = clean_pron($w);
 	
-	$p =~ s/[\.,;:!\?]//g;
-	$p =~ s/'|’/ /g;
-	
-	my $voy = "eaiuUY";
-	$voy .= "ɛÉÈɛ̃œɑ";
-	$voy .= decode('utf8', "ɛÉÈɛ̃œɑə");
-	my $cons = "bdfGklmnpsStvz";
-	$cons .= "ɡʒʁʃ";
-	$cons .= decode('utf8', "ɡʒʁʃ");
+	my $voy = "[eaoAiIuUY]";
+	$voy .= "|" . "ɛ|ɛ̃|É|È|œ|œ̃|ɑ|ɑ̃|ə|ɔ|ɔ̃|ø";
+	$voy .= "|" . decode('utf8',"ɛ|ɛ̃|É|È|œ|œ̃|ɑ|ɑ̃|ə|ɔ|ɔ̃|ø");
+	my $cons = "[cbdfgGklmnpqrRsStTvz]";
+	$cons .= "|" . decode('utf8', "ɡ|ʒ|ʁ|ʃ");
+	my $ei = decode("utf8", "i|É|ə|œ");
+	$ei .= "|e|i|É|ə|œ";
+	my $s = '(\s|^)';
+	my $e = '(\s|$)';
 	
 	# Mots
-	$p =~ s/\b([ldms])es\b/$1ɛ/g;
+	$p =~ s/\b([ldms])es\b/$1ɛ\b/g;
 	$p =~ s/\b([ldms])e\b/$1ə/g;
+	$p =~ s/\buns?\b/œ̃/g;
 	
 	# Terminaisons courantes
-	$p =~ s/tions?\b/SJɔ̃/g;
+	if ($typ eq 'verb' and not $art->{'l_is_locution'}) {
+		$p =~ s/ez\b/É/g;
+		$p =~ s/e(nt|s)?\b//g;
+		$p =~ s/ent\b//g;
+	} else {
+		$p =~ s/tions?\b/SJɔ̃/g;
+	}
+	$p =~ s/er\b/É/g;
 	$p =~ s/(ots?|e?aux?)\b/O/g;
 	$p =~ s/ements?\b/əmɑ̃/g;
 	$p =~ s/ments?\b/mɑ̃/g;
 	$p =~ s/ants?\b/ɑ̃/g;
 	$p =~ s/antes?\b/ɑ̃t/g;
 	$p =~ s/ient?s?\b/Jɛ̃/g;
-	$p =~ s/ets?\b/ɛ/g;
+	$p =~ s/ets?$e/ɛ$1/g;
 	$p =~ s/ettes?\b/ɛT/g;
 	$p =~ s/ès\b/ɛS/g;
 	$p =~ s/iers?\b/JÉ/g;
-	$p =~ s/er\b/É/g;
+	$p =~ s/ompt/ɔ̃t/g;
+	$p =~ s/amps/ɑ̃/g;
+	$p =~ s/on[dt]s?\b/ɔ̃/g;
+	$p =~ s/oses?\b/oz/g;
+	$p =~ s/eux\b/ø/g;
+	$p =~ s/ails?\b/ɑJ/g;
+	$p =~ s/eds?\b/É/g;
+
+	$p =~ s/tionn/SJɔn/g;
+	$p =~ s/euse/øz/g;
 	
-	$p =~ s/s\b//g;
+	$p =~ s/[sx](\s|$)//g;
+	$p =~ s/($voy)[pdt]\b/$1/g;
 	$p =~ s/ç/S/g;
+	$p =~ s/î|ï/I/g;
+	$p =~ s/ô/O/g;
+	$p =~ s/à|â/ɑ/g;
+	$p =~ s/ê|è/ɛ/g;
+	$p =~ s/é/É/g;
+
+	$p =~ s/([trdp])\1/$1/g;
 	
 	# 1 Voyelles
-	$p =~ s/ï/I/g;
-	$p =~ s/é/É/g;
-	$p =~ s/ê|è/ɛ/g;
-	$p =~ s/e([$cons]{2})/ɛ$1/g;
-	$p =~ s/([$voy])s([$voy])/$1z$2/g;	# se -> ze
-	$p =~ s/oy([$voy])/waJ$1/g;
-	$p =~ s/y([$voy])/J$1/g;
-	$p =~ s/y/i/g;
+	$p =~ s/($voy)s($voy)/$1z$2/g;	# se -> ze
+	$p =~ s/oy($voy)/waJ$1/g;
+	$p =~ s/y($voy)/J$1/g;
 	$p =~ s/ou?in/wɛ̃/g;
-	$p =~ s/ou([iaɛ̃ɛ])/w$1/g;
-	$p =~ s/oue([$cons])/wɛ$1/g;
+	$p =~ s/ou([ia]|ɛ̃|ɛ)/w$1/g;
+	$p =~ s/oue($cons)/wɛ$1/g;
 	$p =~ s/oi/wa/g;
-	$p =~ s/euill?e?/œJ/g;
-	$p =~ s/ueill?e?/œJ/g;
-	$p =~ s/ille/iJe/g;
-	$p =~ s/ails?$/ɑJ/gi;
+	$p =~ s/aill/ɑJ/gi;
+	$p =~ s/euill?e?/ŒJ/g;
+	$p =~ s/ueill?e?/ŒJ/g;
+	$p =~ s/(œ|oe)il/ŒJ/g;
+	$p =~ s/œ/É/g;
+	$p =~ s/eill?e?/ɛJ/g;
+	$p =~ s/vill/vil/g;
+	$p =~ s/ill/iJ/g;
+
+	$p =~ s/y/i/g;
 	$p =~ s/eu[xs]?$/ø/g;
-	$p =~ s/(e|œ)u/œ/g;
+	$p =~ s/eu/ø/g;
+	$p =~ s/ø($cons)/œ$1/g;
 	$p =~ s/ou/U/g;
-	$p =~ s/o([$cons])\1/ɔ$1/g;
-	$p =~ s/on([$cons]|s?\b)/ɔ̃$1/g;
-	$p =~ s/à|â/ɑ/g;
-	$p =~ s/e?au|ô/O/g;
+	$p =~ s/onn/ɔn/g;
+	$p =~ s/omm/ɔm/g;
+	$p =~ s/o[nm]($cons|s?\b)/ɔ̃$1/g;
+	$p =~ s/o($cons)/ɔ$1/g;
+	$p =~ s/e?au/O/g;
 	$p =~ s/e?oi/wa/g;
-	$p =~ s/o([^$voy])/ɔ$1/g;
 	#$p =~ s/i([$voy])[^es]/j$1/g;
-	$p =~ s/e([$cons])\1/ɛ$1/g;
-	$p =~ s/an([$cons])/ɑ̃$1/g;
-	$p =~ s/(\b|[$cons])(i?)(ain|en|in)h?(\b|[$cons])/$1$2ɛ̃$4/g;
-	$p =~ s/\b(ain|en|in)([$cons])/ɛ̃$2/g;
-	$p =~ s/(\b|[$cons])(i?)(an)(\b|[$cons])/$1$2ɑ̃$4/g;
-	$p =~ s/iɛ̃/Jɛ̃/g;
-	$p =~ s/iɛ/Jɛ/g;
-	$p =~ s/ia/Ja/g;
+	$p =~ s/ann/An/g;
+	$p =~ s/e([nt])\1/ɛ$1/g;
+	$p =~ s/en($voy)/ən$1/g;
+	$p =~ s/an($voy)/An$1/g;
+	$p =~ s/[ae][nm]($cons)/ɑ̃$1/g;
+	$p =~ s/[ae]n(($cons)?)/ɑ̃$1/g;
+	$p =~ s/(\b|$cons)(i?)(ain|en|in)h?(\b|$cons)/$1$2ɛ̃$4/g;
+	$p =~ s/\b(ain|en|in)($cons)/ɛ̃$2/g;
+	#$p =~ s/(\b|$cons)(i?)(an)(\b|$cons)/$1$2ɑ̃$4/g;
+	$p =~ s/e([t])/ə$1/g;
+	$p =~ s/e(($cons){2})/ɛ$1/g;
+	$p =~ s/e($cons)\1/ɛ$1$1/g;
+	$p =~ s/e([x])/ɛ$1/g;
 	$p =~ s/ai/ɛ/g;
+	$p =~ s/i(ɛ|ɛ̃|É|[aou])/J$1/g;
 	
 	# Consonnes
+	$p =~ s/($voy)sh?($voy)/$1z$2/g;
+	$p =~ s/($voy)cc($ei)/$1ks$2/g;
 	$p =~ s/cc/k/g;
-	$p =~ s/cc([aoɔUiuy])/k$1/g;
-	$p =~ s/cc([eÉiJ])/ks$1/g;
-	$p =~ s/c([lraoɔuU])/k$1/g;
-	$p =~ s/c([eÉiJ])/s$1/g;
+	$p =~ s/cc([aoUiuy]|ɔ)/k$1/g;
+	$p =~ s/cc($ei)/ks$1/g;
+	$p =~ s/c([lraouU]|ɔ)/k$1/g;
+	$p =~ s/c($ei|J)/s$1/g;
 	$p =~ s/(sc|c|s)h/ʃ/g;
 	$p =~ s/c/k/g;
 	$p =~ s/ph/f/g;
-	$p =~ s/gn([$voy])/ɲ$1/g;
-	$p =~ s/g([aoɔUiÉ])/ʒ$1/g;
-	$p =~ s/gu/G/g;
-	$p =~ s/j|ge|g$/ʒ/g;
-	$p =~ s/qu([ei])/k$1/g;
+	$p =~ s/gn($voy)/ɲ$1/g;
+	$p =~ s/g([aoUY]|ɔ)/G$1/g;
+	$p =~ s/g($ei)/ʒ$1/g;
+	$p =~ s/j|ge$/ʒ/g;
+	$p =~ s/g/G/g;
+	$p =~ s/qu($voy)/k$1/g;
 	$p =~ s/x/ks/g;
 	
 	# Doubles
-	$p =~ s/([$cons])\1/$1/g;
-	$p =~ s/([$voy])\1/$1/g;
+	$p =~ s/($cons)\1/$1/g;
+	#$p =~ s/($voy)\1/$1/g;
 	
 	# Derniers
 	$p =~ s/ant?s?\b/ɑ̃/g;
 	$p =~ s/u/Y/g;
-	$p =~ s/([$voy])[ts]\b/$1/g;
+	$p =~ s/Y[iI]/ɥi/g;
+	$p =~ s/($voy)[ts]\b/$1/g;
 	$p =~ s/([^\b]{2})e /$1 /g;
 	$p =~ s/e\b//g;	# e muet
 	$p =~ s/e/ə/g;	# e caduc
@@ -237,7 +345,8 @@ sub pron_in_fr
 sub SAMPA_API
 {
 	my ($w) = @_;
-	$w =~ tr/YUIJGOST/yuijgost/;
+	$w =~ tr/AYUIOJGST/ayuiojgst/;
+	$w =~ s/Œ/œ/g;
 	$w =~ s/r/ʁ/g;
 	$w =~ s/g/ɡ/g;
 	$w =~ s/É/e/g;
